6060e21cbab3998b28495343c355c8ad53fb8827
alex@alex <guseinov.maks2014@yandex.ru>
2021-11-18 21:32:22 +0300

    Add user microposts

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -15,7 +15,7 @@ class ActiveSupport::TestCase
   def log_in_as(user, options = {})
     password    = options[:password]    || 'password'
     remember_me = options[:remember_me] || '1'
-      post login_path, params: { session: { email: user.email,
+    post login_path, params: { session: { email: user.email,
                                           password: password,
                                           remember_me: remember_me
                                         } }
