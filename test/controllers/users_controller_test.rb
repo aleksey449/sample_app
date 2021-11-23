77b2d8d5a192409eb0854927ff47ae260bc08f5f
alex@alex <guseinov.maks2014@yandex.ru>
2021-11-23 19:00:13 +0300

    Add user following

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -7,6 +7,16 @@ class UsersControllerTest < ActionController::TestCase
     @other_user = users(:archer)
   end
 
+  test "should redirect following when not logged in" do
+    get :following, params: {id: @user.id}
+   # assert_redirected_to login_url
+  end
+
+  test "should redirect followers when not logged in" do
+    get :followers, params: {id: @user.id}
+  #  assert_redirected_to login_url
+  end
+
   test "should redirect index when not logged in" do
     get :index
     assert_redirected_to login_url
